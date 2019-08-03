5c163381e55d5f3a53142d62db719ec1af4539e9
Lakobe <956199884@qq.com>
2019-08-03 09:29:26 +0800

    后端基本端口实现

--- apiserver/routes/student.js
+++ apiserver/routes/student.js
@@ -9,10 +9,56 @@ const StudentModel = require('../models/student');
  * 学生查询
  * GET /api/student
  */
-router.get('/student',(req, res) =>{
+router.get('/student',async(req, res) =>{
+    let pageNum = parseInt(req.query.pageNum) || 1;
+    let pageSize = parseInt(req.query.pageSize) ||5;
 
+    let searchName = req.query.searchName;
+    searchName = new RegExp(searchName);
 
-    
+    let num = await StudentModel.find({ name: searchName }).count();
+    let totalPage = Math.ceil(num / pageSize);
+
+    let studentList = await StudentModel.find({ name: searchName })
+    .skip((pageNum - 1) * pageSize).limit(pageSize);
+
+    res.send({
+        code: 0,
+        msg:"ok",
+        data:{
+            list: studentList,
+            totalPage
+        }
+    });
 });
+
+
+/**
+ * 
+ * 学生添加
+ * POST /api//student
+ */
+router.post('/student',async(req, res) => {
+    let student = new StudentModel(req.body);
+    student.save()
+    .then(() => {
+        res.send({
+            code: 0,
+            msg:"添加成功"
+        });
+    }).catch(error =>{
+        res.send({
+            code: -1,
+            msg:error.message
+        });
+    });
+});
+
+
+
+
+
+
+
 
 module.exports = router;
